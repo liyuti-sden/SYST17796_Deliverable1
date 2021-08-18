3143f0cf5dc90324d8004ff9747365b3705f5018
liyuti-sden <84794580+liyuti-sden@users.noreply.github.com>
2021-08-17 20:39:47 -0400

    Add files via upload

---
+++ src/ca/sheridancollege/project/BlackJackCard.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author YutingLi
+ */
+public class BlackJackCard extends Card{
+    private String suit; //clubs, spades, diamonds, hearts
+    private int value;//1-13
+
+    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String toString(){
+        return this.suit+"_"+this.value; 
+    }
+    
+}
